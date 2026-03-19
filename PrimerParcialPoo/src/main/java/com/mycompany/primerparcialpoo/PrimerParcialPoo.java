d0b7f37c9bdd734fa7641d15c825a5fce4e5f762
JhojanCamiloOsorio <jhosoriog@unal.edu.co>
2026-03-19 08:55:22 -0500

    Create PrimerParcialPoo.java

---
+++ PrimerParcialPoo/src/main/java/com/mycompany/primerparcialpoo/PrimerParcialPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primerparcialpoo;
+
+/**
+ *
+ * @author Jhojan Camilo Osorio, Juan Esteban Valencia
+ */
+public class PrimerParcialPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
